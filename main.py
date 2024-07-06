12d37837f8fe4dfe788628cf01fc5fae3af550cd
linoliumknife <shayne.cotner26@gmail.com>
2024-07-05 20:38:56 -0700

    added letter counter and made prettier output

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
-def read(file_contents):
+
+#count words in book
+def word_counter(file_contents):
     words = file_contents.split()
     count = len(words)
     return count
 
 
-def main():
-    with open("books/frankenstein.txt") as f:
+
+#count letters in book
+def char_counter(file_contents):
+    chars = {}
+    lowered_string = file_contents.lower()
+
+    for char in lowered_string:
+        if char.isalpha():  
+            if char in chars:
+                chars[char] += 1
+            else:
+                chars[char] = 1
+
+    # Print each character and its count
+    for char, count in chars.items():
+        print(f"Character '{char}' found {count} times.")
+
+
+def main(book_title):
+    with open(f"books/{book_title}.txt") as f:
         file_contents = f.read()
+
+    print(f"--- Begin report of books/{book_title}.txt ---")
+
+    word_count = word_counter(file_contents)
+    print(f"Total words in {book_title}: {word_count}")
+
+    char_counter(file_contents)
     
-    word_count = read(file_contents)
-    print (word_count)
+    
+    print(f"--- End report of books/{book_title}.txt ---")
+  
     
 
 if __name__ == "__main__":
-     main()
+     main("frankenstein")
